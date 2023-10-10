f3fefc2cba8e60eb6e3a3179417a4f1a0a05860c
GGjahoon <Zx_jahoon@outlook.com>
2023-10-10 16:04:00 +0800

    use viper load config

--- main.go
+++ main.go
@@ -4,25 +4,23 @@ import (
 	"database/sql"
 	"github.com/GGjahoon/MySimpleBank/api"
 	db "github.com/GGjahoon/MySimpleBank/db/sqlc"
+	"github.com/GGjahoon/MySimpleBank/util"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	driverName = "postgres"
-	dbSource   = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
-	address    = "127.0.0.1:8080"
-)
-
 func main() {
-	var err error
-	conn, err := sql.Open(driverName, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config :", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(address)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("server cannot start", err)
 	}
